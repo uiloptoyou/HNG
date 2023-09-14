3fc65b9917da0e7bdb3fb4082b8123e488a3e6b8
Tolu Joel <toluisjoel@gmail.com>
2023-09-14 10:39:47 +0100

    Updated rest framework rendering class

--- stage_2/base/settings.py
+++ stage_2/base/settings.py
@@ -136,3 +136,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Rest framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': 'rest_framework.renderers.JSONRenderer',
+}
